db69654c9a22a2bce3f94bb91d2ed609ab3091b0
Joshua Obee <139596208+Pour1ng@users.noreply.github.com>
2025-12-10 00:09:22 -0500

    Simplify response by removing escapeHtml function
    Removed HTML escaping from search query response.

--- xss-examples/example3_safe.js
+++ xss-examples/example3_safe.js
@@ -1,11 +1,5 @@
-const escapeHtml = s => s
-  .replace(/&/g, '&amp;')
-  .replace(/</g, '&lt;')
-  .replace(/>/g, '&gt;')
-  .replace(/"/g, '&quot;')
-  .replace(/'/g, '&#x27;');
-
 app.get('/search', (req, res) => {
-  const q = escapeHtml(req.query.q || '');
-  res.send(`<h1>Results for ${q}</h1>`);
+  const q = req.query.q || ''; 
+  res.type('text/plain');
+  res.send(`Results for ${q}`); 
 });
